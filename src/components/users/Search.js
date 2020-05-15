b37980c0aab5ff1f9aecc984c3c54447f0b2d16b
rick-love <ricklove@outlook.com>
2020-05-15 22:16:39 +0200

    added alert and search users

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
   state = {
     text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
   };
   onChange = (event) =>
     this.setState({ [event.target.name]: event.target.value });
 
   onSubmit = (event) => {
     event.preventDefault();
+    if(this.state.text === '') {
+      this.props.setAlert('Please enter data', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+  
+    }
   };
 
   render() {
+
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className='form' action=''>
@@ -28,6 +44,14 @@ export class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear && ( 
+          <button
+            className='btn btn-light btn-block'
+            onClick={clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
